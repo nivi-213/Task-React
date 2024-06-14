22036c43fc0c1749d79158ab07497a28a6684c1b
Nivetha <nivethamurugaiyan1999@gmail.com>
2024-06-14 17:10:33 +0530

    admin auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import FormModal from "./component/Authentication/FormModal";
+
 import LoginForm from "./component/Authentication/login/LoginForm";
 import SignupForm from "./component/Authentication/signup/SignupForm";
 import Registration from "./component/Form/Registration";
-import ProtectedRoute from "./component/Authentication/ProtectedRoute";
+
 import UserTable from "./component/Form/UserTable";
 
 const App = () => {
